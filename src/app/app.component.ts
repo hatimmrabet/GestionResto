e4ebf05880f1cfc333cd99481508149c8fca45f2
Hatim M'rabet El Khomssi <mrabet.hatim2018@gmail.com>
2022-03-01 16:26:26 +0100

    affichage des appareils

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'MyRestaurant';
+  title = 'My Restaurant';
+  isAuth = false;
+
+  app1 = 'App 1 1';
+  app2 = 'App 22';
+  app3 = 'App 33';
+
+  appareils = [
+    {
+      name: 'Machine à laver',
+      status: 'allumer',
+    },
+    {
+      name: 'machine a cafe',
+      status: 'éteint',
+    },
+    {
+      name: 'xbox',
+      status: 'allumer',
+    },
+  ];
+
+  constructor() {
+    setTimeout(() => {
+      this.isAuth = true;
+    }, 3000);
+  }
+
+  onAllumer() {
+    console.log('on allume tout');
+  }
 }
