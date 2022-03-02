1f7fdee8c72107a3baec7bab68bf6d5d13ea5b0d
Hatim M'rabet El Khomssi <mrabet.hatim2018@gmail.com>
2022-03-02 23:19:10 +0100

    affichage des produits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,32 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'My Restaurant';
-  isAuth = false;
-
-  appareils = [
-    {
-      name: 'Machine à laver',
-      status: 'allumer',
-    },
-    {
-      name: 'machine a cafe',
-      status: 'éteint',
-    },
-    {
-      name: 'xbox',
-      status: 'allumer',
-    },
-  ];
-
-  constructor() {
-    setTimeout(() => {
-      this.isAuth = true;
-    }, 3000);
-  }
-
-  onAllumer() {
-    console.log('on allume tout');
-  }
-}
+export class AppComponent {}
