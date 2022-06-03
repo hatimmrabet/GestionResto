bc07ce4550df22b38cc986515473ef53af0dab4a
Hatim M'rabet El Khomssi <mrabet.hatim2018@gmail.com>
2022-06-03 13:08:48 +0200

    edit my profil

--- src/app/components/profil/profil.component.ts
+++ src/app/components/profil/profil.component.ts
@@ -1,15 +1,96 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ERole } from 'src/app/models/ERole.model';
+import { User } from 'src/app/models/user.model';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
+import { UsersService } from 'src/app/services/users.service';
+import { SignUpValidator } from 'src/app/validators/signup.validator';
 
 @Component({
   selector: 'app-profil',
   templateUrl: './profil.component.html',
-  styleUrls: ['./profil.component.scss']
+  styleUrls: ['./profil.component.scss'],
 })
 export class ProfilComponent implements OnInit {
+  signupForm: FormGroup;
+  hide = true;
+  selected = null;
+  alertSignup: { type: string; message: string };
+  ERole = ERole;
+  loading = true;
+  canChangePassword = false;
 
-  constructor() { }
+  constructor(
+    private usersService: UsersService,
+    private formBuilder: FormBuilder,
+    private tokenStorageService: TokenStorageService,
+  ) {}
 
   ngOnInit(): void {
+    this.alertSignup = { type: '', message: '' };
+    this.initSignupForm();
   }
 
+  initSignupForm() {
+    this.usersService.getUser(this.tokenStorageService.getUser()!!.id).subscribe((data) => {
+      // console.log(data);
+      this.signupForm = this.formBuilder.group({
+        firstName: [data.firstName, [Validators.required]],
+        lastName: [data.lastName, [Validators.required]],
+        email: [data.email, [Validators.required, Validators.email]],
+        address: [data.address, [Validators.required]],
+        phoneNumber: [
+          data.phoneNumber,
+          [Validators.required, Validators.pattern('^0[0-9]{9}$')],
+        ], // phone number should have 10 digits
+        role: [data.role, [Validators.required]],
+        birthDate: [
+          new Date(data.birthDate),
+          [Validators.required, SignUpValidator.validDate],
+        ], // birthdat should be in the past
+      });
+      this.loading = false;
+    });
+  }
+
+  get signupControl() {
+    return this.signupForm.controls;
+  }
+
+  onSubmitSignupForm() {
+    const user = new User(this.signupForm.value);
+    user.id = this.tokenStorageService.getUser()!!.id;
+    this.usersService.editUser(user).subscribe(
+      (response: any) => {
+        // console.log(response);
+        this.alertSignup = {
+          type: 'success',
+          message: 'User Modified successfully',
+        };
+        setTimeout(() => {
+          window.location.reload();
+        }, 1500);
+      },
+      (error) => {
+        // console.log(error);
+        this.alertSignup = { type: 'danger', message: error.error.response };
+      }
+    );
+  }
+
+  changePassword() {
+    this.canChangePassword = !this.canChangePassword;
+    if (this.canChangePassword) {
+      this.signupForm.addControl(
+        'password',
+        this.formBuilder.control('', [
+          Validators.required,
+          Validators.minLength(6),
+        ])
+      );
+    } else {
+      this.signupForm.removeControl('password');
+    }
+  }
 }
