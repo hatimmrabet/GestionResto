8c9417d10621a7b2e5124f1f51cf33a5c60649d3
Hatim M'rabet El Khomssi <mrabet.hatim2018@gmail.com>
2022-06-03 12:24:05 +0200

    admin can change or not the password

--- src/app/components/edit-user/edit-user.component.ts
+++ src/app/components/edit-user/edit-user.component.ts
@@ -3,7 +3,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ERole } from 'src/app/models/ERole.model';
 import { User } from 'src/app/models/user.model';
-import { AuthService } from 'src/app/services/auth.service';
 import { UsersService } from 'src/app/services/users.service';
 import { SignUpValidator } from 'src/app/validators/signup.validator';
 
@@ -21,15 +20,14 @@ export class EditUserComponent implements OnInit, OnDestroy {
   selected = null;
   alertSignup: { type: string; message: string };
   ERole = ERole;
-  loading = true
+  loading = true;
+  canChangePassword = false;
 
   constructor(
     private _Activatedroute: ActivatedRoute,
     private usersService: UsersService,
-    private authService : AuthService,
     private formBuilder: FormBuilder
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
     this.sub = this._Activatedroute.paramMap.subscribe((params) => {
@@ -50,12 +48,16 @@ export class EditUserComponent implements OnInit, OnDestroy {
         firstName: [data.firstName, [Validators.required]],
         lastName: [data.lastName, [Validators.required]],
         email: [data.email, [Validators.required, Validators.email]],
-        password: ['', [Validators.required, Validators.minLength(6)]],
         address: [data.address, [Validators.required]],
-        phoneNumber: [data.phoneNumber,[Validators.required, Validators.pattern('^0[0-9]{9}$')],
+        phoneNumber: [
+          data.phoneNumber,
+          [Validators.required, Validators.pattern('^0[0-9]{9}$')],
         ], // phone number should have 10 digits
         role: [data.role, [Validators.required]],
-        birthDate: [new Date(data.birthDate), [Validators.required, SignUpValidator.validDate]], // birthdat should be in the past
+        birthDate: [
+          new Date(data.birthDate),
+          [Validators.required, SignUpValidator.validDate],
+        ], // birthdat should be in the past
       });
       this.loading = false;
     });
@@ -71,12 +73,27 @@ export class EditUserComponent implements OnInit, OnDestroy {
     this.usersService.editUser(user).subscribe(
       (response: any) => {
         // console.log(response);
-        this.alertSignup = { type: 'success', message: 'User Modified successfully' };
+        this.alertSignup = {
+          type: 'success',
+          message: 'User Modified successfully',
+        };
+        setTimeout(() => {
+          window.location.reload();
+        }, 1500);
       },
       (error) => {
         // console.log(error);
         this.alertSignup = { type: 'danger', message: error.error.response };
       }
     );
+  }
+
+  changePassword() {
+    this.canChangePassword = !this.canChangePassword;
+    if(this.canChangePassword) {
+      this.signupForm.addControl('password', this.formBuilder.control('', [Validators.required, Validators.minLength(6)]));
+    } else {
+      this.signupForm.removeControl('password');
+    }
   }
 }
